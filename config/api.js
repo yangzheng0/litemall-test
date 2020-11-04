50fb2561d4e59b2813e81e4884d3707b7ba73880
yangzheng <yz6780610>
2020-11-05 00:10:52 +0800

    首页优惠券页面
    首页优惠券页面

--- config/api.js
+++ config/api.js
@@ -3,6 +3,7 @@
 var WxApiRoot = 'http://localhost:9080/wx/';
 
 module.exports = {
+  IndexUrl: WxApiRoot + 'home/index', //首页数据接口
   AuthLoginByAccount: WxApiRoot + 'auth/login', //账号登录
   AuthLogout: WxApiRoot+'auth/logout' // 退出登录
 }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,18 @@
 //index.js
 //获取应用实例
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
+const user = require('../../utils/user.js');
+
+//获取应用实例
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    banner:[],
+    channel:[],
+    coupon:[],
+    goodsCount:0
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,39 +21,18 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    this.getIndexData();
+  },
+  getIndexData: function() {
+    let that = this
+    util.request(api.IndexUrl).then(function(res){
+      if (res.errno === 0) {
+        that.setData({
+          banner:res.data.banner,
+          channel:res.data.channel,
+          coupon: res.data.couponList
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })
