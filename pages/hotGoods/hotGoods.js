4c929fcbab11fb3bf007c723b885deb45de46e16
yangzheng <yz6780610>
2020-12-08 01:28:10 +0800

    人气推荐

--- pages/hotGoods/hotGoods.js
+++ pages/hotGoods/hotGoods.js
@@ -1,11 +1,16 @@
 // pages/hotGoods/hotGoods.js
+const util = require('../../utils/util.js');
+const api = require('../../config/api.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    bannerInfo:{
+      'imgUrl':'/static/images/hot.png',
+      'name':'大家都在买的'
+    },
   },
 
   /**
@@ -61,6 +66,46 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+  openSortFilter:function(event) {
+    let currentId = event.currentTarget.id;
+    switch(currentId) {
+      case 'categoryFilter':
+        this.setData({
+          categoryFilter: !this.data.categoryFilter,
+          currentSortType: 'category',
+          currentSort: 'add_time',
+          currentSortOrder: 'desc'
+        });
+        break;
+      case 'priceSort':
+        let tmpSortOrder = 'asc';
+        if (this.data.currentSortOrder == 'asc') {
+          tmpSortOrder = 'desc';
+        }
+        this.setData({
+          currentSortType: 'price',
+          currentSort: 'retail_price',
+          currentSortOrder: tmpSortOrder,
+          categoryFilter: false
+        });
+
+        this.getGoodsList();
+        break;
+      default:
+        //综合排序
+        this.setData({
+          currentSortType: 'default',
+          currentSort: 'add_time',
+          currentSortOrder: 'desc',
+          categoryFilter: false,
+          categoryId: 0,
+        });
+        this.getGoodsList();
+    }
+  },
+  getGoodsList:function(){
 
   }
 })
